f7420a7959a008fc15965cfa688ab5b285049d54
ysccoding <richard.has.yan2@gmail.com>
2022-01-10 11:22:51 -0800

    Modifying add/get balance to use Compound Functions

--- InterestBearingBankAccount.sol
+++ InterestBearingBankAccount.sol
@@ -10,7 +10,7 @@ interface cETH {
     function redeem(uint redeemTokens) external returns (uint); // Withdrawal or Redeem from Compound
 
     function exchangeRateStored() external view returns (uint); // Calculates exchange rate from Ctoken
-    function balanceOf(address owner) external view returns (uint256 balance); // Gets Token Balance of Given Address
+    function balanceOf(address owner) external view returns (uint256 balance); // Gets cETH Token Balance of Given Address
 }
 
 contract InterestBearingBankAccount {
@@ -30,16 +30,22 @@ contract InterestBearingBankAccount {
     
     // Adds balance to contract. Timestamps used for Interest
     function addBalance() public payable {
-        balances[msg.sender] = msg.value;
-        totalContractBalance = totalContractBalance + msg.value;
-        depositTimestamps[msg.sender] = block.timestamp;
+        // Sned Ether for Minting
+        ceth.mint{value: msg.value};
+
+        // balances[msg.sender] = msg.value;
+        // totalContractBalance = totalContractBalance + msg.value;
+        // depositTimestamps[msg.sender] = block.timestamp;
     }
     
     // Get balance of any provided address
     function getBalance(address userAddress) public view returns(uint) {
-        uint principal = balances[userAddress];
-        uint timeElapsed = block.timestamp - depositTimestamps[userAddress]; // Time in Seconds
-        return principa l + uint((principal * 10 * timeElapsed) / (100 * 365 * 24 * 60 * 60)); // Simple interest of 0.1%  per year
+        
+        return ceth.balanceOf(userAddress) * ceth.exchangeRateStored() / 1e18; // Compound exchange rate is scaled by 1e18
+        
+        // uint principal = balances[userAddress];
+        // uint timeElapsed = block.timestamp - depositTimestamps[userAddress]; // Time in Seconds
+        // return principal + uint((principal * 10 * timeElapsed) / (100 * 365 * 24 * 60 * 60)); // Simple interest of 0.1%  per year
     }
     
     // Withdraw All Money in Contract for Sender
